23ed8736e2a1f44fe2c6865332e06c479aa87de1
Jshsjsbd <pjan6308@gmail.com>
2025-06-21 11:37:06 +0300

    secret (11)

--- api/beacon-logs.ts
+++ api/beacon-logs.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { db } from '../lib/firebase';
+import { db } from './firebase';
 import { ref, get, remove } from 'firebase/database';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
--- api/beacon.ts
+++ api/beacon.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { db } from '../lib/firebase';
+import { db } from './firebase';
 import { ref, push } from 'firebase/database';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
